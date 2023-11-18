d51a7fed6c764443e444d068448e64669198ed06
notmucht0it <kevinmonahan01@gmail.com>
2023-11-17 22:45:05 -0600

    Added basic parts of the database to the models.py file.Moved all first name, last name, home address, phone number, and email address in the user database. I did not really see the point in having these a part of the TA, instructor, and admin databases when they all need these things.

--- TAScheduler/models.py
+++ TAScheduler/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email_address = models.Charfield(max_length=90) # This is also the username
+    password = models.Charfield(max_length=30)
+    first_name = models.Charfield(max_length=30)
+    last_name = models.Charfield(max_length=30)
+    home_address = models.Charfield(max_length=90)
+    phone_number = models.IntegerField()
+
+class TA(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    sections = models.ManyToManyField(Section)
+
+class Instructor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    sections = models.ManyToManyField(Section)
+
+class Administrator(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+class Course(models.Model):
+    course_id = models.IntegerField()
+    semester = models.Charfield(max_length=11)
+
+
+class Section(models.Model):
+    section_id = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    location = models.Charfield(max_length=30)
+    meeting_time = models.DateTimeField()
+    credits = models.IntegerField()
