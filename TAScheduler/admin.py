b9a3c624fbb9eab535deeef13271631f43f2e69c
notmucht0it <kevinmonahan01@gmail.com>
2023-11-20 10:25:58 -0600

    Added all the models to the admin site, so we can create items for the database in the admin site

--- TAScheduler/admin.py
+++ TAScheduler/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from TAScheduler.models import User, TA, Course, Section, Instructor, Administrator, Lab, Lecture, TAToCourse, \
+    InstructorToCourse
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(TA)
+admin.site.register(Course)
+admin.site.register(Section)
+admin.site.register(Instructor)
+admin.site.register(Administrator)
+admin.site.register(Lab)
+admin.site.register(Lecture)
+admin.site.register(TAToCourse)
+admin.site.register(InstructorToCourse)
