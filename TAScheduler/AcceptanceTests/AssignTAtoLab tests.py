a9db863b67a4cc3a30566a9ab8755b258cc12606
Josiah DeYong <jpdeyong@uwm.edu>
2023-11-21 10:25:08 -0600

    Fixed Format

--- TAScheduler/AcceptanceTests/AssignTAtoLab tests.py
+++ TAScheduler/AcceptanceTests/AssignTAtoLab tests.py
@@ -4,8 +4,9 @@ from django.test import TestCase
 
 from TAScheduler.models import User, TA, Course, TAToCourse
 
+
 #
-class AssignTAToCourse(TestCase):
+class SuccessfulAssignment(TestCase):
     user = None
     ta = None
     course = None
@@ -28,18 +29,90 @@ class AssignTAToCourse(TestCase):
         self.assertEqual(temp.course, self.course, "TAToCourse does not make correct course")
         self.assertEqual(temp.ta, self.ta, "TAToCourse does not make correct TA")
 
+
+class NoTA(TestCase):
+    user = None
+    ta = None
+    course = None
+    tatocourse = None
+
+    def setUp(self):
+        self.user = User.objects.create(email_address="ta@class.com", password="password", first_name="first",
+                                        last_name="last", home_address="there", phone_number=1234567890)
+
+        self.ta = TA.objects.create(user=self.user)
+
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
+                                            description="test", num_of_sections=2, modality="online", credits=1)
+
+        self.tatocourse = TAToCourse.objects.create(ta=self.ta, course=self.course)
+
     def test_no_ta(self):
         temp = TAToCourse.objects.create(ta=self.ta)
         self.assertIsNone(temp.ta, "TAToCourse makes TA with no input")
+
+
+class NoCourse(TestCase):
+    user = None
+    ta = None
+    course = None
+    tatocourse = None
+
+    def setUp(self):
+        self.user = User.objects.create(email_address="ta@class.com", password="password", first_name="first",
+                                        last_name="last", home_address="there", phone_number=1234567890)
+
+        self.ta = TA.objects.create(user=self.user)
+
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
+                                            description="test", num_of_sections=2, modality="online", credits=1)
+
+        self.tatocourse = TAToCourse.objects.create(ta=self.ta, course=self.course)
 
     def test_no_course(self):
         temp = TAToCourse.objects.create(course=self.course)
         self.assertIsNone(temp.course, "TAToCourse makes course with no input")
 
+
+class TaDelete(TestCase):
+    user = None
+    ta = None
+    course = None
+    tatocourse = None
+
+    def setUp(self):
+        self.user = User.objects.create(email_address="ta@class.com", password="password", first_name="first",
+                                        last_name="last", home_address="there", phone_number=1234567890)
+
+        self.ta = TA.objects.create(user=self.user)
+
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
+                                            description="test", num_of_sections=2, modality="online", credits=1)
+
+        self.tatocourse = TAToCourse.objects.create(ta=self.ta, course=self.course)
+
     def test_ta_deletion(self):
         temp = TAToCourse.objects.create(ta=self.ta, course=self.course)
         self.ta.delete()
         self.assertIsNone(temp, "TAToCourse link should have deleted when TA was deleted")
+
+
+class CourseDelete(TestCase):
+    user = None
+    ta = None
+    course = None
+    tatocourse = None
+
+    def setUp(self):
+        self.user = User.objects.create(email_address="ta@class.com", password="password", first_name="first",
+                                        last_name="last", home_address="there", phone_number=1234567890)
+
+        self.ta = TA.objects.create(user=self.user)
+
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
+                                            description="test", num_of_sections=2, modality="online", credits=1)
+
+        self.tatocourse = TAToCourse.objects.create(ta=self.ta, course=self.course)
 
     def test_course_deletion(self):
         temp = TAToCourse.objects.create(ta=self.ta, course=self.course)
--- TAScheduler/AcceptanceTests/InstructorToCourse tests.py
+++ TAScheduler/AcceptanceTests/InstructorToCourse tests.py
@@ -3,12 +3,12 @@ from django.test import TestCase
 from TAScheduler.models import User, Instructor, Course, InstructorToCourse
 
 
-class InstructorToCourseTests(TestCase):
+class SuccessfulCreation(TestCase):
     user = None
     instructor = None
     course = None
     instrToCourse = None
-#
+
     def setUp(self):
         self.user = User.objects.create(email_address="instructor@class.com", password="password", first_name="first",
                                         last_name="last", home_address="there", phone_number=1234567890)
@@ -27,13 +27,70 @@ class InstructorToCourseTests(TestCase):
         self.assertEqual(temp.instructor, self.instructor, "Instructor is not correct instructor")
         self.assertEqual(temp.course, self.course, "Course is not correct course")
 
+
+class NoInstructor(TestCase):
+    user = None
+    instructor = None
+    course = None
+    instrToCourse = None
+
+    def setUp(self):
+        self.user = User.objects.create(email_address="instructor@class.com", password="password",
+                                        first_name="first", last_name="last", home_address="there",
+                                        phone_number=1234567890)
+
+        self.instructor = Instructor.objects.create(user=self.user)
+
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
+                                            description="test", num_of_sections=2, modality="online", credits=1)
+
+        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
+
     def test_no_instructor(self):
         temp = InstructorToCourse.objects.create(course=self.course)
         self.assertIsNone(temp.instructor, "No instructor has instructor")
 
+
+class NoCourse(TestCase):
+    user = None
+    instructor = None
+    course = None
+    instrToCourse = None
+
+    def setUp(self):
+        self.user = User.objects.create(email_address="instructor@class.com", password="password",
+                                        first_name="first",
+                                        last_name="last", home_address="there", phone_number=1234567890)
+
+        self.instructor = Instructor.objects.create(user=self.user)
+
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
+                                            description="test", num_of_sections=2, modality="online", credits=1)
+
+        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
+
     def test_no_course(self):
         temp = InstructorToCourse.objects.create(instructor=self.instructor)
         self.assertIsNone(temp.course, "No course has course")
+
+
+class InstructorNoRoom(TestCase):
+    user = None
+    instructor = None
+    course = None
+    instrToCourse = None
+
+    def setUp(self):
+        self.user = User.objects.create(email_address="instructor@class.com", password="password",
+                                        first_name="first",
+                                        last_name="last", home_address="there", phone_number=1234567890)
+
+        self.instructor = Instructor.objects.create(user=self.user)
+
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
+                                            description="test", num_of_sections=2, modality="online", credits=1)
+
+        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
 
     def test_instructor_no_room(self):
         tempinstructor = Instructor.objects.create(user=self.user, max_assignments=0)
@@ -43,10 +100,48 @@ class InstructorToCourseTests(TestCase):
         # InstructorToCourse.objects has error: Expected type 'Iterable | Container', got 'Manager' instead
         self.assertNotIn(temp, InstructorToCourse.objects, "InstructorToCourse was made when instructor at max courses")
 
+
+class InstructorDeletion(TestCase):
+    user = None
+    instructor = None
+    course = None
+    instrToCourse = None
+
+    def setUp(self):
+        self.user = User.objects.create(email_address="instructor@class.com", password="password",
+                                        first_name="first",
+                                        last_name="last", home_address="there", phone_number=1234567890)
+
+        self.instructor = Instructor.objects.create(user=self.user)
+
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
+                                            description="test", num_of_sections=2, modality="online", credits=1)
+
+        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
+
     def test_instructor_deletion(self):
         temp = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
         self.instructor.delete()
         self.assertIsNone(temp, "Instructor to Course link should have deleted when instructor was deleted")
+
+
+class CourseDeletion(TestCase):
+    user = None
+    instructor = None
+    course = None
+    instrToCourse = None
+
+    def setUp(self):
+        self.user = User.objects.create(email_address="instructor@class.com", password="password",
+                                        first_name="first",
+                                        last_name="last", home_address="there", phone_number=1234567890)
+
+        self.instructor = Instructor.objects.create(user=self.user)
+
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
+                                            description="test", num_of_sections=2, modality="online", credits=1)
+
+        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
 
     def test_course_deletion(self):
         temp = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
