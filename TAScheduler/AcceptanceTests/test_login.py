b9b408c2ea951855c2e4718e8f7f2e8cf63251b0
Alec Brookens <alecbrookens@gmail.com>
2023-11-21 14:24:08 -0600

    Updated as per reviews

--- TAScheduler/AcceptanceTests/test_login.py
+++ TAScheduler/AcceptanceTests/test_login.py
@@ -2,12 +2,13 @@ from django.test import TestCase, Client
 from TAScheduler.models import User
 
 class SuccessfulLogin(TestCase):
+    client = None
     def setUp(self):
         self.client = Client()
         User.objects.create_user(email_address="user@example.com", password="password")
 
     def test_login_with_valid_credentials(self):
-        response = self.client.post('/login/', {'email': 'user@example.com', 'password': 'password'})
+        response = self.client.post('/', {'email': 'user@example.com', 'password': 'password'})
         self.assertRedirects(response, '/home/')  # Assuming the user is redirected to home page on successful login
 class InvalidLoginIncorrectPassword(TestCase):
     def setUp(self):
@@ -15,12 +16,12 @@ class InvalidLoginIncorrectPassword(TestCase):
         User.objects.create_user(email_address="user@example.com", password="password")
 
     def test_login_with_incorrect_password(self):
-        response = self.client.post('/login/', {'email': 'user@example.com', 'password': 'wrongpassword'})
+        response = self.client.post('/', {'email': 'user@example.com', 'password': 'wrongpassword'})
         self.assertFormError(response, 'form', None, 'Invalid username or password')  # Assuming this error is shown
 class InvalidLoginNonexistentUser(TestCase):
     def setUp(self):
         self.client = Client()
 
     def test_login_with_nonexistent_user(self):
-        response = self.client.post('/login/', {'email': 'nonexistent@example.com', 'password': 'password'})
+        response = self.client.post('/', {'email': 'nonexistent@example.com', 'password': 'password'})
         self.assertFormError(response, 'form', None, 'Invalid username or password')  # Assuming this error is shown
--- TAScheduler/AcceptanceTests/test_logout.py
+++ TAScheduler/AcceptanceTests/test_logout.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-
 class LogoutTest(TestCase):
+    client = None
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='12345')
@@ -9,11 +9,9 @@ class LogoutTest(TestCase):
 
     def test_logout(self):
         # User is logged in here
-        response = self.client.get('/logout/')  # Assuming /logout/ is the URL for the logout view
-        self.assertRedirects(response, '/login/')  # Assuming user is redirected to login page after logout
+        response = self.client.get('/home/')  # Assuming /logout/ is the URL for the logout view
+        self.assertRedirects(response, '/')  # Assuming user is redirected to login page after logout
 
         # Check if the user session has been cleared
         self.assertNotIn('_auth_user_id', self.client.session)
 
-        # Optionally, you can also check if the session cookie has been invalidated
-        # This depends on how your Django application handles cookies post-logout
