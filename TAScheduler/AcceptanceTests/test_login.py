e1775c3cc1a124f0fa4b82e7fd42589326401d25
Alec Brookens <alecbrookens@gmail.com>
2023-11-21 14:53:03 -0600

    Updated as per reviews again

--- TAScheduler/AcceptanceTests/test_login.py
+++ TAScheduler/AcceptanceTests/test_login.py
@@ -14,14 +14,21 @@ class InvalidLoginIncorrectPassword(TestCase):
     def setUp(self):
         self.client = Client()
         User.objects.create_user(email_address="user@example.com", password="password")
-
     def test_login_with_incorrect_password(self):
         response = self.client.post('/', {'email': 'user@example.com', 'password': 'wrongpassword'})
         self.assertFormError(response, 'form', None, 'Invalid username or password')  # Assuming this error is shown
+    def test_login_with_empty_password(self):
+        response = self.client.post('/', {'email': 'user@example.com', 'password': ''})
+        self.assertFormError(response, 'form', None, 'Password field cannot be empty')
 class InvalidLoginNonexistentUser(TestCase):
     def setUp(self):
         self.client = Client()
-
     def test_login_with_nonexistent_user(self):
         response = self.client.post('/', {'email': 'nonexistent@example.com', 'password': 'password'})
         self.assertFormError(response, 'form', None, 'Invalid username or password')  # Assuming this error is shown
+    def test_login_with_empty_username(self):
+        response = self.client.post('/', {'email': '', 'password': 'password'})
+        self.assertFormError(response, 'form', None, 'Username field cannot be empty')
+    def test_login_with_empty_username_and_password(self):
+        response = self.client.post('/', {'email': '', 'password': ''})
+        self.assertFormError(response, 'form', None, 'Username and Password fields cannot be empty')
