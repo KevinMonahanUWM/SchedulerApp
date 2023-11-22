e577083af21b9921d65d3ce91ed2fa301ed02527
Josiah DeYong <jpdeyong@uwm.edu>
2023-11-21 23:34:14 -0600

    Redid InstructorToCourse tests.py

--- TAScheduler/AcceptanceTests/InstructorToCourse tests.py
+++ TAScheduler/AcceptanceTests/InstructorToCourse tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from TAScheduler.models import User, Instructor, Course, InstructorToCourse
 
@@ -7,143 +7,82 @@ class SuccessfulCreation(TestCase):
     user = None
     instructor = None
     course = None
-    instrToCourse = None
+    instructorToCourse = None
 
     def setUp(self):
-        self.user = User.objects.create(email_address="instructor@class.com", password="password", first_name="first",
-                                        last_name="last", home_address="there", phone_number=1234567890)
+        self.user = Client()
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
 
-        self.instructor = Instructor.objects.create(user=self.user)
+        self.instructor = Instructor.objects.create(user=temp)
+        self.instructor.save()
 
-        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
-                                            description="test", num_of_sections=2, modality="online", credits=1)
+        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="testCourse", description="test", num_of_sections=3, modality="online", credits=3)
+        self.course.save()
 
-        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
+        self.instructorToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
 
     # /home/managecourse/addinstructor
     def test_creation(self):
-        temp = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
-        self.assertIsNotNone(temp, "Did not make InstructorToCourse")
-        self.assertEqual(temp.instructor, self.instructor, "Instructor is not correct instructor")
-        self.assertEqual(temp.course, self.course, "Course is not correct course")
-
+        self.user.post("/home/managecourse/addinstructor", {"selection", self.instructor}, follow=True)
+        response = self.user.post("/home/managecourse/addinstructor/courseselect", {"selection", self.course}, follow=True)
+        self.assertEquals(self.instructorToCourse, User.objects.get(self.instructorToCourse), "Did not successfully create account")
+        self.assertRedirects(response, '/home/managecourse/addinstructor/courseselect/success')
 
 class NoInstructor(TestCase):
     user = None
-    instructor = None
-    course = None
-    instrToCourse = None
-
     def setUp(self):
-        self.user = User.objects.create(email_address="instructor@class.com", password="password",
-                                        first_name="first", last_name="last", home_address="there",
-                                        phone_number=1234567890)
-
-        self.instructor = Instructor.objects.create(user=self.user)
-
-        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
-                                            description="test", num_of_sections=2, modality="online", credits=1)
-
-        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
-
+        self.user = Client()
     def test_no_instructor(self):
-        temp = InstructorToCourse.objects.create(course=self.course)
-        self.assertIsNone(temp.instructor, "No instructor has instructor")
-
+        resp = self.user.get("/home/managecourse/addinstructor")
+        self.assertEquals(resp.context["message"], "No Existing Instructors to Assign", "Cannot assign instructor when none exist")
 
 class NoCourse(TestCase):
     user = None
     instructor = None
-    course = None
-    instrToCourse = None
-
     def setUp(self):
-        self.user = User.objects.create(email_address="instructor@class.com", password="password",
-                                        first_name="first",
-                                        last_name="last", home_address="there", phone_number=1234567890)
+        self.user = Client()
 
-        self.instructor = Instructor.objects.create(user=self.user)
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
 
-        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
-                                            description="test", num_of_sections=2, modality="online", credits=1)
-
-        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
-
+        self.instructor = Instructor.objects.create(user=temp)
+        self.instructor.save()
     def test_no_course(self):
-        temp = InstructorToCourse.objects.create(instructor=self.instructor)
-        self.assertIsNone(temp.course, "No course has course")
-
+        resp = self.user.post("/home/managecourse/addinstructor", {"selection", self.instructor}, follow=True)
+        self.assertEquals(resp.context["message"], "No existing Courses to assign",
+                          "Cannot assign courses when none exist")
 
 class InstructorNoRoom(TestCase):
     user = None
     instructor = None
-    course = None
-    instrToCourse = None
-
     def setUp(self):
-        self.user = User.objects.create(email_address="instructor@class.com", password="password",
-                                        first_name="first",
-                                        last_name="last", home_address="there", phone_number=1234567890)
+        self.user = Client()
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
 
-        self.instructor = Instructor.objects.create(user=self.user)
-
-        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
-                                            description="test", num_of_sections=2, modality="online", credits=1)
-
-        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
+        self.instructor = Instructor.objects.create(user=temp, max_assignments=0)
+        self.instructor.save()
 
     def test_instructor_no_room(self):
-        tempinstructor = Instructor.objects.create(user=self.user, max_assignments=0)
-        temp = InstructorToCourse.objects.create(instructor=tempinstructor, course=self.course)
+        resp = self.user.post("/home/managecourse/addinstructor", {"selection", self.instructor}, follow=True)
+        self.assertEquals(resp.context["message"], "Instructor has max assignments",
+                          "Cannot assign courses when instructor is at max")
 
-        # should fail and not make I think but IDK how
-        # InstructorToCourse.objects has error: Expected type 'Iterable | Container', got 'Manager' instead
-        self.assertNotIn(temp, InstructorToCourse.objects, "InstructorToCourse was made when instructor at max courses")
-
-
-class InstructorDeletion(TestCase):
+class SuccessfulTransfer(TestCase):
     user = None
     instructor = None
-    course = None
-    instrToCourse = None
-
     def setUp(self):
-        self.user = User.objects.create(email_address="instructor@class.com", password="password",
-                                        first_name="first",
-                                        last_name="last", home_address="there", phone_number=1234567890)
+        self.user = Client()
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
 
-        self.instructor = Instructor.objects.create(user=self.user)
-
-        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
-                                            description="test", num_of_sections=2, modality="online", credits=1)
-
-        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
-
-    def test_instructor_deletion(self):
-        temp = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
-        self.instructor.delete()
-        self.assertIsNone(temp, "Instructor to Course link should have deleted when instructor was deleted")
-
-
-class CourseDeletion(TestCase):
-    user = None
-    instructor = None
-    course = None
-    instrToCourse = None
-
-    def setUp(self):
-        self.user = User.objects.create(email_address="instructor@class.com", password="password",
-                                        first_name="first",
-                                        last_name="last", home_address="there", phone_number=1234567890)
-
-        self.instructor = Instructor.objects.create(user=self.user)
-
-        self.course = Course.objects.create(course_id=100, semester="fall 2023", name="test",
-                                            description="test", num_of_sections=2, modality="online", credits=1)
-
-        self.instrToCourse = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
-
-    def test_course_deletion(self):
-        temp = InstructorToCourse.objects.create(instructor=self.instructor, course=self.course)
-        self.course.delete()
-        self.assertIsNone(temp, "Instructor to Course link should have deleted when course was deleted")
+        self.instructor = Instructor.objects.create(user=temp)
+        self.instructor.save()
+    def test_instructor_to_next(self):
+        resp = self.user.post("/home/managecourse/addinstructor", {"selection", self.instructor}, follow=True)
+        self.assertEquals(resp.context["instructor"], self.instructor, "Instructor not transerred over properly")
