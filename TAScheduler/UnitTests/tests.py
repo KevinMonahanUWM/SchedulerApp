52b5c01f18a211a1f393bd66e4b671a9a4357cb4
notmucht0it <kevinmonahan01@gmail.com>
2023-11-22 14:33:00 -0600

    Added tests for deleting a section

--- TAScheduler/UnitTests/tests.py
+++ TAScheduler/UnitTests/tests.py
@@ -1,7 +1,9 @@
+import datetime
+
 from django.test import TestCase
 
-from TAScheduler.models import Course, User, TA
-from TAScheduler.views_methods import CourseObj, AdminObj, TAObj
+from TAScheduler.models import Course, User, TA, Section, Lab
+from TAScheduler.views_methods import CourseObj, AdminObj, TAObj, LabObj
 
 
 # PBI Assignments ...
@@ -103,12 +105,12 @@ class TestAdminRemoveAccount(TestCase):  # Kevin
 
     def setUp(self):
         self.hold_user = User.objects.create(
-                email_address='kev@example.com',
-                password='kevpassword',
-                first_name='Kevin',
-                last_name='User',
-                home_address='123 Kevin St',
-                phone_number='1234667890'
+            email_address='kev@example.com',
+            password='kevpassword',
+            first_name='Kevin',
+            last_name='User',
+            home_address='123 Kevin St',
+            phone_number='1234667890'
         )
         temp_ta = TA.objects.create(user=self.hold_user, grader_status=False)
         self.tempTA = TAObj(temp_ta)
@@ -125,21 +127,69 @@ class TestAdminRemoveAccount(TestCase):  # Kevin
         self.admin = AdminObj(hold_admin)
 
     def test_successful_delete(self):
-        self.admin.removeCourse(self.tempTA)
+        self.admin.removeUser(self.tempTA)
         self.assertNotIn(User.objects, self.hold_user, "Did not remove user from the database")
 
     def test_delete_null_user(self):
         User.delete(self.hold_user)
         with self.assertRaises(RuntimeError, msg="Tried to delete a non-existent user"):
-            self.admin.removeCourse(self.tempTA)
+            self.admin.removeUser(self.tempTA)
 
     def test_delete_non_user(self):
         with self.assertRaises(TypeError, msg="Tried to delete not a user"):
-            self.admin.removeCourse(11)
+            self.admin.removeUser(11)
 
 
 class TestAdminRemoveSection(TestCase):  # Kevin
-    pass
+    hold_sec = None
+    admin = None
+    tempLab = None
+
+    def setUp(self):
+        temp_course = Course.objects.create(
+            course_id=101,
+            semester='Fall 2023',
+            name='Introduction to Testing',
+            description='A course about writing tests in Django.',
+            num_of_sections=3,
+            modality='Online',
+            credits=4
+        )
+        self.hold_sec = Section.objects.create(
+            section_id=1011,
+            course=temp_course,
+            location="A distant realm",
+            meeting_time=datetime.datetime
+        )
+        temp_lab = Lab.objects.create(
+            section=self.hold_sec,
+            ta=None
+        )
+        self.tempLab = LabObj(temp_lab)
+        temp = User.objects.create(
+            email_address='admin@example.com',
+            password='adminpassword',
+            first_name='Admin',
+            last_name='User',
+            home_address='123 Admin St',
+            phone_number='1234567890'
+        )
+        hold_admin = Admin(user=temp)
+        hold_admin.save()
+        self.admin = AdminObj(hold_admin)
+
+    def test_successful_delete(self):
+        self.admin.removeSection(self.tempLab)
+        self.assertNotIn(User.objects, self.hold_sec, "Did not remove section from the database")
+
+    def test_delete_null_user(self):
+        Section.delete(self.hold_sec)
+        with self.assertRaises(RuntimeError, msg="Tried to delete a non-existent section"):
+            self.admin.removeSection(self.tempLab)
+
+    def test_delete_non_user(self):
+        with self.assertRaises(TypeError, msg="Tried to delete not a section"):
+            self.admin.removeSection(11)
 
 
 class TestAdminEditCourse(TestCase):  # Kevin
