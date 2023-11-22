6246cb8f5983ef6bdfdf491979f5bc0bbd145eb0
Kiran-Ee <earnsha2@uwm.edu>
2023-11-22 03:15:41 -0600

    11/22/23
    I added all the unit test methods, stubs, into "tests.py" ... I tried to assign people to their specifc PBIs but it did not distribute evenly. So, I just gave an even spread.

--- TAScheduler/UnitTests/tests.py
+++ TAScheduler/UnitTests/tests.py
@@ -1,4 +1,97 @@
-from django.test import TestCase
+import unittest
+# PBI Assignments ...
+# Alec = #1,#2 (Total = 6)
+# Kevin = #3,#4,#5 (Total = 4)
+# Randall = #6,#7,#8 (Total = 12)
+# Kiran = #9,#10,#11 (Total = 15)
+# Joe = #12,#13 (Total = 8)
+# SEE METHOD DESCRIPTIONS FOR GUIDE ON HOW TO WRITE.
+# Feel free to make suggestions on discord (add/remove/edit methods)!
+### Rememeber: These methods were made before any coding (I was guessing) so it's likely they should be changed.
+class TestUserLogin(unittest.TestCase): #Alec
+    pass
+class TestUserGetID(unittest.TestCase): #Alec
+    pass
+class TestUserGetPassword(unittest.TestCase): #Alec
+    pass
+class TestUserGetName(unittest.TestCase): #Alec
+    pass
+class TestUserGetRole(unittest.TestCase): #Alec
+    pass
+class TestAdminCreateCourse(unittest.TestCase): #Alec
+    pass
+class TestAdminCreateUser(unittest.TestCase): #Alec
+    pass
+class TestAdminCreateSection(unittest.TestCase): #Alec
+    pass
+class TestAdminRemoveCourse(unittest.TestCase): #Kevin
+    pass
+class TestAdminRemoveAccount(unittest.TestCase): #Kevin
+    pass
+class TestAdminRemoveSection(unittest.TestCase): #Kevin
+    pass
+class TestAdminEditCourse(unittest.TestCase): #Kevin
+    pass
+class TestAdminEditSection(unittest.TestCase): #Kevin
+    pass
+class TestAdminEditAccount(unittest.TestCase): #Kevin
+    pass
+class TestAdminCourseInstrAsgmt(unittest.TestCase): #Kevin
+    pass
+class TestAdminLabTAAsgmt(unittest.TestCase): #Kevin
+    pass
+class TestTAHasMaxAsgmts(unittest.TestCase): #Kiran
+    pass
+class TestTAAssignTACourse(unittest.TestCase): #Kiran
+    pass
+class TestTAGetTACrseAsgmts(unittest.TestCase):#Kiran
+    pass
+class TestTAGetTALabAsgmts(unittest.TestCase): #Kiran
+    pass
+class TestTAGetGraderStatus(unittest.TestCase): #Kiran
+    pass
+class TestInstructorHasMaxAsgmts(unittest.TestCase): #Kiran
+    pass
+class TestInstructorAssignInstrCourse(unittest.TestCase): #Kiran
+    pass
+class TestInstructorGetInstrCrseAsgmts(unittest.TestCase): #Kiran
+    pass
+class TestCourseAddInstructor(unittest.TestCase): #Randall
+    pass
+class TestCourseAddTA(unittest.TestCase): #Randall
+    pass
+class TestCourseRemoveAssignment(unittest.TestCase): #Randall
+    pass
+class TestCourseRemoveCourse(unittest.TestCase): #Randall
+    pass
+class TestCourseEditCourseInfo(unittest.TestCase): #Randall
+    pass
+class TestCourseGetAsgmtsForCrse(unittest.TestCase): #Randall
+    pass
+class TestCourseGetSectionsForCrse(unittest.TestCase): #Randall
+    pass
+class TestCourseGetCrseInfo(unittest.TestCase): #Randall
+    pass
+class TestSectionGetID(unittest.TestCase): #Joe
+    pass
+class TestSectionGetParentCourse(unittest.TestCase): #Joe
+    pass
+class TestLabGetLabTAAsgmt(unittest.TestCase): #Joe
+    pass
+class TestLabAddTA(unittest.TestCase): #Joe
+    pass
+class TestLabRemoveTA(unittest.TestCase): #Joe
+    pass
+class TestLectureGetLecInstrAsgmt(unittest.TestCase): #Joe
+    pass
+class TestLectureAddInstructor(unittest.TestCase): #Joe
+    pass
+class TestLectureRemoveInstructor(unittest.TestCase): #Joe
+    pass
 
-# Create your tests here.
-# Unit tests per view sections
+
+
+
+
+
+
