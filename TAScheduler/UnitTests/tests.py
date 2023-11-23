68f9cfadd1bcbe926c61501072e59e0939f742f2
Josiah DeYong <jpdeyong@uwm.edu>
2023-11-23 02:59:18 -0600

    Bad tests will fix in morning but just in case I don't get to it

--- TAScheduler/UnitTests/tests.py
+++ TAScheduler/UnitTests/tests.py
@@ -1,4 +1,10 @@
 import unittest
+import datetime
+
+from django.test import TestCase
+
+from TAScheduler.models import Course, User, TA, Section, Lab, Administrator, Instructor, InstructorToCourse
+from TAScheduler.views_methods import CourseObj, AdminObj, TAObj, LabObj, SectionObj, InstructorObj, LectureObj
 
 
 # PBI Assignments ...
@@ -32,6 +38,7 @@ class TestUserGetRole(unittest.TestCase):  # Alec
 
 class TestAdminInit(unittest.TestCase):
     pass
+
 
 class TestAdminCreateCourse(unittest.TestCase):  # Alec
     pass
@@ -76,8 +83,10 @@ class TestAdminCourseInstrAsgmt(unittest.TestCase):  # Kevin
 class TestAdminCourseTAAsgmt(unittest.TestCase):  # Kevin
     pass
 
+
 class TestTAInit(unittest.TestCase):
     pass
+
 
 class TestTAHasMaxAsgmts(unittest.TestCase):  # Kiran
     pass
@@ -110,6 +119,7 @@ class TestTAGetTALecAsgmts(unittest.TestCase):  # Kiran
 class TestTAGetGraderStatus(unittest.TestCase):  # Kiran
     pass
 
+
 class TestInstrutorInit(unittest.TestCase):
     pass
 
@@ -141,11 +151,12 @@ class TestInstructorLecTAAsmgt(unittest.TestCase):
 class TestInstructorLabTAAsmgt(unittest.TestCase):
     pass
 
+
 class TestCourseInit(unittest.TestCase):
     pass
 
 
-class TestCourseAddInstructor(unittest.TestCase):  # Randall
+class TestCourseAddInstructor(TestCase):  # Randall
     pass
 
 
@@ -178,50 +189,505 @@ class TestCourseGetCrseInfo(unittest.TestCase):  # Randall
 
 
 class TestSectionGetID(unittest.TestCase):  # Joe
-    pass
+    section = None
+    course = None
+
+    def setUp(self):
+        self.course = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        self.course.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=self.course,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+        self.section = SectionObj(hold_section)
+
+    def test_get_id(self):
+        self.assertEquals(self.section.getID(), 1011, "getID() did not retrieve correct section_id")
+
+    # Maybe test for section_id = None, but I don't think section can be made without
 
 
 class TestSectionGetParentCourse(unittest.TestCase):  # Joe
-    pass
+    section = None
+    course = None
+
+    def setUp(self):
+        self.course = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        self.course.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=self.course,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+        self.section = SectionObj(hold_section)
+
+    def test_get_parent_course(self):
+        self.assertEquals(self.section.getParentCourse(), self.course,
+                          "getParentCourse() did not retrieve correct course")
 
 
 class TestLabInit(unittest.TestCase):
-    pass
+    section = None
+    course = None
+    lab = None
+
+    def setUp(self):
+        self.course = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        self.course.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=self.course,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+
+        self.section = SectionObj(hold_section)
+        self.lab = LabObj(self.section)
+
+    def test_lab_make(self):
+        self.assertIsNotNone(self.lab.__init__(), "__init__ failed in making Lab")
+
 
 class TestLabGetLabTAAsgmt(unittest.TestCase):  # Joe
-    pass
+    lab = None
+    ta = None
+
+    def setUp(self):
+        tmpcourse = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        tmpcourse.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=tmpcourse,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+        hold_section_lab = SectionObj(hold_section)
+        self.lab = LabObj(hold_section_lab)
+
+    def test_get_ta(self):
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
+
+        self.ta = TA.objects.create(user=temp)
+        self.ta.save()
+
+        self.assertEquals(self.ta, self.lab.getLabTAAsgmt(), "getLabTAAsgmt() does not retrieve ta")
+
+    def test_no_ta(self):
+        self.assertIsNone(self.lab.getLabTAAsgmt(), "getLabTAAsgmt() finds something with no TA")
 
 
 class TestLabAddTA(unittest.TestCase):  # Joe
-    pass
+    lab = None
+    ta = None
+
+    def setUp(self):
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
+
+        self.ta = TA.objects.create(user=temp)
+        self.ta.save()
+
+        tmpcourse = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        tmpcourse.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=tmpcourse,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+        hold_section_lab = SectionObj(hold_section)
+        self.lab = LabObj(hold_section_lab)
+
+    def test_add_ta(self):
+        temp2 = User(email_address="test2@test.com", password="password2", first_name="first2", last_name="last2",
+                     home_address="Your mom's house", phone_number=1234567890)
+        temp2.save()
+
+        tempta = TA.objects.create(user=temp2)
+        tempta.save()
+        self.lab.addTA(tempta)
+        self.assertIn(tempta, self.lab, "addTA() does not add TA")  # Can't write tests without container
+
+    def test_full(self):
+        temp2 = User(email_address="test2@test.com", password="password2", first_name="first2", last_name="last2",
+                     home_address="Your mom's house", phone_number=1234567890)
+        temp2.save()
+
+        tempta = TA.objects.create(user=temp2)
+        tempta.save()
+        self.lab.addTA(tempta)
+        with self.assertRaises(RuntimeError, msg="Tried to add TA to full Lab"):
+            self.lab.addTA(self.ta)
 
 
 class TestLabRemoveTA(unittest.TestCase):  # Joe
-    pass
+    lab = None
+    ta = None
+
+    def setUp(self):
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
+
+        self.ta = TA.objects.create(user=temp)
+        self.ta.save()
+
+        tmpcourse = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        tmpcourse.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=tmpcourse,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+        hold_section_lab = SectionObj(hold_section)
+        self.lab = LabObj(hold_section_lab)
+
+    def test_remove(self):
+        self.lab.removeTA()
+        self.assertNotIn(self.ta, self.lab, "removeTA() does not remove TA")  # Can't write tests without container
 
 
 class TestLectureInit(unittest.TestCase):
-    pass
+    section = None
+    course = None
+    lecture = None
+
+    def setUp(self):
+        self.course = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        self.course.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=self.course,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+
+        tempsection = SectionObj(hold_section)
+        self.lecture = LectureObj(tempsection)
+
+    def test_lecture_make(self):
+        self.assertIsNotNone(self.lecture.__init__(), "__init__ failed in making Lecture")
+
 
 class TestLectureGetLecInstrAsgmt(unittest.TestCase):  # Joe
-    pass
+    instructor = None
+    lecture = None
+
+    def setUp(self):
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
+
+        self.instructor = Instructor.objects.create(user=temp)
+        self.instructor.save()
+
+        tmpcourse = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        tmpcourse.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=tmpcourse,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+
+        tempsection = SectionObj(hold_section)
+        self.lecture = LectureObj(tempsection)
+
+    def test_get_instructor(self):
+        self.assertEquals(self.instructor, self.lecture.getLecInstrAsmgt(), "getLecInstrAsmgt() does not return correct")
+
+    def test_get_but_none(self):
+        with self.assertRaises(RuntimeError, msg="Tried to retrieve from none"):
+            self.lecture.getLecInstrAsmgt()
+
 
 
 class TestLectureAddInstructor(unittest.TestCase):  # Joe
-    pass
+    instructor = None
+    lecture = None
 
+    def setUp(self):
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
+
+        self.instructor = Instructor.objects.create(user=temp)
+        self.instructor.save()
+
+        tmpcourse = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        tmpcourse.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=tmpcourse,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+
+        tempsection = SectionObj(hold_section)
+        self.lecture = LectureObj(tempsection)
+
+    def test_add_but_full(self):
+        self.lecture.addInstr(self.instructor)
+    def test_add(self):
+        self.lecture.addInstr(self.instructor)
+        self.assertIsNotNone(self.lecture.getLecInstrAsmgt(), "getLecInstrAsmgt() Did not add instructor")
 
 class TestLectureRemoveInstructor(unittest.TestCase):  # Joe
-    pass
+    instructor = None
+    lecture = None
 
+    def setUp(self):
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
 
+        self.instructor = Instructor.objects.create(user=temp)
+        self.instructor.save()
+
+        tmpcourse = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        tmpcourse.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=tmpcourse,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+
+        tempsection = SectionObj(hold_section)
+        self.lecture = LectureObj(tempsection)
+
+    def test_none_to_remove(self):
+        with self.assertRaises(RuntimeError, msg="removeInstr() Tried to remove from none"):
+            self.lecture.removeInstr()
+    def test_remove(self):
+        self.lecture.removeInstr()
+        self.assertIsNone(self.lecture.getLecInstrAsmgt(), "removeInstr() did not remove")
 class TestLectureGetLecTAAsgmt(unittest.TestCase):  # Joe
-    pass
+    ta = None
+    lecture = None
+
+    def setUp(self):
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
+
+        self.ta = TA.objects.create(user=temp)
+        self.ta.save()
+
+        tmpcourse = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        tmpcourse.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=tmpcourse,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+
+        tempsection = SectionObj(hold_section)
+        self.lecture = LectureObj(tempsection)
+
+    def test_get_ta_assignment(self):
+        self.assertEquals(self.ta, self.lecture.getLectureTAAsgmt(),
+                          "getLectureTAAsgmt() does not return correct")
+    def test_get_no_ta_assignment(self):
+        with self.assertRaises(RuntimeError, msg="getLectureTAAsgmt() Tried to retrieve from none"):
+            self.lecture.getLectureTAAsgmt()
 
 
 class TestLectureAddTA(unittest.TestCase):  # Joe
-    pass
+    ta = None
+    lecture = None
+
+    def setUp(self):
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
+
+        self.ta = TA.objects.create(user=temp)
+        self.ta.save()
+
+        tmpcourse = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        tmpcourse.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=tmpcourse,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+
+        tempsection = SectionObj(hold_section)
+        self.lecture = LectureObj(tempsection)
+
+    def test_add(self):
+        self.lecture.addTA(self.ta)
+    def test_add_but_full(self):
+        self.lecture.addTA(self.ta)
+        self.assertIsNotNone(self.lecture.getLectureTAAsgmt(), "getLectureTAAsgmt() Did not add instructor")
 
 
 class TestLectureRemoveTA(unittest.TestCase):  # Joe
-    pass
+    ta = None
+    lecture = None
+
+    def setUp(self):
+        temp = User(email_address="test@test.com", password="password", first_name="first", last_name="last",
+                    home_address="Your mom's house", phone_number=1234567890)
+        temp.save()
+
+        self.ta = TA.objects.create(user=temp)
+        self.ta.save()
+
+        tmpcourse = Course.objects.create(
+            course_id=100,
+            semester="fall 2023",
+            name="testCourse",
+            description="test",
+            num_of_sections=3,
+            modality="online",
+            credits=3
+        )
+        tmpcourse.save()
+
+        hold_section = Section.objects.create(
+            section_id=1011,
+            course=tmpcourse,
+            location="Cool place",
+            meeting_time=datetime.datetime
+        )
+        hold_section.save()
+
+        tempsection = SectionObj(hold_section)
+        self.lecture = LectureObj(tempsection)
+    def test_none_to_remove(self):
+        with self.assertRaises(RuntimeError, msg="removeTA() Tried to remove from none"):
+            self.lecture.removeTA()
+    def test_remove(self):
+        self.lecture.removeTA()
+        self.assertIsNone(self.lecture.getLectureTAAsgmt(), "removeTA() did not remove")
