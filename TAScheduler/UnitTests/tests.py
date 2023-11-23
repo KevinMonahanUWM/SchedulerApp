0032a3e0866f4d8206cc6794c23d0fc5dae37d62
Randall Sanders <randallds2@gmail.com>
2023-11-23 00:16:45 -0600

    added unit tests

--- TAScheduler/UnitTests/tests.py
+++ TAScheduler/UnitTests/tests.py
@@ -1,4 +1,9 @@
+import datetime
+
 from django.test import TestCase
+
+from TAScheduler.models import Course, User, TA, Section, Lab, Administrator, InstructorToCourse, TAToCourse
+from TAScheduler.views_methods import CourseObj, AdminObj, TAObj, LabObj, InstructorObj
 
 
 # PBI Assignments ...
@@ -32,6 +37,7 @@ class TestUserGetRole(TestCase):  # Alec
 
 class TestAdminInit(TestCase):
     pass
+
 
 class TestAdminCreateCourse(TestCase):  # Alec
     pass
@@ -76,8 +82,10 @@ class TestAdminCourseInstrAsgmt(TestCase):  # Kevin
 class TestAdminCourseTAAsgmt(TestCase):  # Kevin
     pass
 
+
 class TestTAInit(TestCase):
     pass
+
 
 class TestTAHasMaxAsgmts(TestCase):  # Kiran
     pass
@@ -110,6 +118,7 @@ class TestTAGetTALecAsgmts(TestCase):  # Kiran
 class TestTAGetGraderStatus(TestCase):  # Kiran
     pass
 
+
 class TestInstrutorInit(TestCase):
     pass
 
@@ -141,40 +150,235 @@ class TestInstructorLecTAAsmgt(TestCase):
 class TestInstructorLabTAAsmgt(TestCase):
     pass
 
+
 class TestCourseInit(TestCase):
     pass
 
 
 class TestCourseAddInstructor(TestCase):  # Randall
-    pass
+    def setUp(self):
+        self.hold_course = Course.objects.create(
+            course_id=101,
+            semester='Fall 2023',
+            name='Introduction to Testing',
+            description='A course about writing tests in Django.',
+            num_of_sections=3,
+            modality='Online',
+            credits=4
+        )
+        self.tempCourse = CourseObj(self.hold_course)
+        self.instructor_user = User.objects.create(
+            email_address='instructor@example.com',
+            password='password',
+            first_name='Instructor',
+            last_name='User',
+            home_address='123 Instructor St',
+            phone_number='1234567891'
+        )
+        self.instructor = InstructorObj(self.instructor_user)
+
+    def test_add_instructor(self):
+        self.tempCourse.addInstructor(self.instructor)
+        # Check if the instructor was added to the course
+
+        instructor_to_course_exists = InstructorToCourse.objects.filter(
+            instructor=self.instructor, course=self.course
+        ).exists()
+        self.assertTrue(instructor_to_course_exists, "Instructor was not added to the course")
 
 
 class TestCourseAddTA(TestCase):  # Randall
-    pass
+    def setUp(self):
+        self.hold_course = Course.objects.create(
+            course_id=102,
+            semester='Spring 2024',
+            name='Advanced Testing',
+            description='A course about advanced testing methods.',
+            num_of_sections=2,
+            modality='Hybrid',
+            credits=3
+        )
+        self.tempCourse = CourseObj(self.hold_course)
+        self.ta_user = User.objects.create(
+            email_address='ta@example.com',
+            password='password',
+            first_name='TA',
+            last_name='Assistant',
+            home_address='123 TA St',
+            phone_number='1234567892'
+        )
+        self.ta = TA.objects.create(user=self.ta_user, grader_status=False)
+
+    def test_add_ta(self):
+        self.tempCourse.addTa(self.ta)
+        # Check if the TA was added to the course
+
+        ta_to_course_exists = TAToCourse.objects.filter(
+            ta=self.ta, course=self.hold_course
+        ).exists()
+        self.assertTrue(ta_to_course_exists, "TA was not added to the course")
 
 
 class TestCourseRemoveAssignment(TestCase):  # Randall
-    pass
+
+    def setUp(self):
+        self.hold_course = Course.objects.create(
+            course_id=103,
+            semester='Summer 2024',
+            name='Intermediate Testing',
+            description='Intermediate course on testing.',
+            num_of_sections=2,
+            modality='In-person',
+            credits=3
+        )
+        self.tempCourse = CourseObj(self.hold_course)
+        self.user = User.objects.create(
+            email_address='user@example.com',
+            password='password',
+            first_name='Regular',
+            last_name='User',
+            home_address='123 User St',
+            phone_number='1234567893'
+        )
+
+    def test_remove_assignment(self):
+        self.tempCourse.removeAssignment(self.user)
+        # Check if the assignment (user) was removed from the course
+        assignments = self.tempCourse.getAsgmtsForCrse()
+        self.assertNotIn(self.user, assignments, "User was not removed from the course assignments")
 
 
 class TestCourseRemoveCourse(TestCase):  # Randall
-    pass
+    def setUp(self):
+        self.hold_course = Course.objects.create(
+            course_id=104,
+            semester='Winter 2025',
+            name='Basic Testing',
+            description='Basic course on testing.',
+            num_of_sections=1,
+            modality='Remote',
+            credits=2
+        )
+        self.tempCourse = CourseObj(self.hold_course)
+
+    def test_remove_course(self):
+        self.tempCourse.removeCourse()
+        # Check if the course was removed
+        course_exists = Course.objects.filter(id=self.hold_course.id).exists()
+        self.assertFalse(course_exists, "Course was not removed")
 
 
 class TestCourseEditCourseInfo(TestCase):  # Randall
-    pass
+    def setUp(self):
+        self.hold_course = Course.objects.create(
+            course_id=105,
+            semester='Fall 2025',
+            name='Testing Fundamentals',
+            description='A course on testing fundamentals.',
+            num_of_sections=4,
+            modality='Mixed',
+            credits=3
+        )
+        self.tempCourse = CourseObj(self.hold_course)
+        self.new_info = {"semester": "Spring 2026", "name": "Advanced Testing Fundamentals", "description": "new",
+                         "num_of_section": 3, "modality": "Online", "credits": 6}
+
+    def test_edit_course_info(self):
+        self.tempCourse.editCourse(self.new_info)
+        self.hold_course.refresh_from_db()
+        self.assertEqual(self.hold_course.semester, self.new_info["semester"], "Course semester was not updated")
+        self.assertEqual(self.hold_course.name, self.new_info["name"], "Course name was not updated")
+        self.assertEqual(self.hold_course.description, self.new_info["description"],
+                         "Course description was not updated")
+        self.assertEqual(self.hold_course.num_of_sections, self.new_info["num_of_sections"], "Course section was not updated")
+        self.assertEqual(self.hold_course.modality, self.new_info["modality"], "Course modality was not updated")
+        self.assertEqual(self.hold_course.credits, self.new_info["credits"], "Course credits was not updated")
 
 
 class TestCourseGetAsgmtsForCrse(TestCase):  # Randall
-    pass
+
+    def setUp(self):
+        self.hold_course = Course.objects.create(
+            course_id=101,
+            semester='Fall 2023',
+            name='Introduction to Testing',
+            description='A course about writing tests in Django.',
+            num_of_sections=3,
+            modality='Online',
+            credits=4
+        )
+        self.tempCourse = CourseObj(self.hold_course)
+        # Example: Add some assignments to the course
+        self.user1 = User.objects.create(
+            email_address='user1@example.com',
+            password='password1',
+            first_name='Regular1',
+            last_name='User1',
+            home_address='123 User1 St',
+            phone_number='1234567893'
+        )
+        self.user2 = User.objects.create(
+            email_address='user2@example.com',
+            password='password',
+            first_name='Regular',
+            last_name='User',
+            home_address='123 User St',
+            phone_number='1234567893'
+        )
+
+    def test_get_assignments_for_course(self):
+        assignments = self.tempCourse.getAsgmtsForCrse()
+        # Assertions to verify the correct assignments are returned
+        self.assertIn(self.user1, assignments, "User1 is not in the course assignments")
+        self.assertIn(self.user2, assignments, "User2 is not in the course assignments")
 
 
 class TestCourseGetSectionsForCrse(TestCase):  # Randall
-    pass
+
+    def setUp(self):
+        self.hold_course = Course.objects.create(
+            course_id=102,
+            semester='Spring 2024',
+            name='Advanced Testing',
+            description='A course about advanced testing methods.',
+            num_of_sections=2,
+            modality='Hybrid',
+            credits=3
+        )
+        self.tempCourse = CourseObj(self.hold_course)
+        # Add some sections to the course
+        self.section1 = Section.objects.create(section_id=201, course=self.hold_course)
+        self.section2 = Section.objects.create(section_id=202, course=self.hold_course)
+
+    def test_get_sections_for_course(self):
+        sections = self.tempCourse.getSectionsCrse()
+        self.assertIn(self.section1, sections, "Section1 is not in the course sections")
+        self.assertIn(self.section2, sections, "Section2 is not in the course sections")
 
 
 class TestCourseGetCrseInfo(TestCase):  # Randall
-    pass
+
+    def setUp(self):
+        self.hold_course = Course.objects.create(
+            course_id=103,
+            semester='Winter 2025',
+            name='Basic Testing',
+            description='Basic course on testing.',
+            num_of_sections=1,
+            modality='Remote',
+            credits=2
+        )
+        self.tempCourse = CourseObj(self.hold_course)
+
+    def test_get_course_info(self):
+        info = self.tempCourse.getCrseInfo()
+        # Assertions to verify the correct course info is returned
+        self.assertEqual(info['course_id'], self.hold_course.course_id, "Course ID is incorrect")
+        self.assertEqual(info['name'], self.hold_course.name, "Course name is incorrect")
+        self.assertEqual(info['description'], self.hold_course.description, "Course description is incorrect")
+        self.assertEqual(info['num_of_section'], self.hold_course.num_of_sections, "Course section is incorrect")
+        self.assertEqual(info['modality'], self.hold_course.modality, "Course modality is incorrect")
+        self.assertEqual(info['credits'], self.hold_course.credits, "Course credits is incorrect")
 
 
 class TestSectionGetID(TestCase):  # Joe
@@ -187,6 +391,7 @@ class TestSectionGetParentCourse(TestCase):  # Joe
 
 class TestLabInit(TestCase):
     pass
+
 
 class TestLabGetLabTAAsgmt(TestCase):  # Joe
     pass
@@ -202,6 +407,7 @@ class TestLabRemoveTA(TestCase):  # Joe
 
 class TestLectureInit(TestCase):
     pass
+
 
 class TestLectureGetLecInstrAsgmt(TestCase):  # Joe
     pass
