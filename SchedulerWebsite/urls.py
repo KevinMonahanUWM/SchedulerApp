f7127807fc038e06ab1f92d3d181755c1735523d
Josiah DeYong <jpdeyong@uwm.edu>
2023-11-28 22:09:22 -0600

    Please check choose_section_add_user.html specifically the form ty

--- SchedulerWebsite/urls.py
+++ SchedulerWebsite/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 
 from TAScheduler.views import Home, CourseManagement, CreateCourse, DeleteCourse, EditCourse, AddInstructorToCourse, \
     Login, AccountManagement, CreateAccount, DeleteAccount, EditAccount, SectionManagement, CreateSection, EditSection, \
-    AddUserToSection, DeleteSection, Error, Success, AddTAToSectionSuccess
+    AddUserToSection, DeleteSection, Error, Success, ChooseSectionForUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,6 +39,7 @@ urlpatterns = [
     path('home/managesection/delete', DeleteSection.as_view()),
     path('home/managesection/edit', EditSection.as_view()),
     path('home/managesection/adduser', AddUserToSection.as_view()),
+    path('home/managesection/adduser/choosesection', ChooseSectionForUser.as_view()),
     path('home/error', Error.as_view()),
     path('home/success', Success.as_view())
 ]
