370d3f4dacb71a85aa74b47c897b9c87d3779143
notmucht0it <kevinmonahan01@gmail.com>
2023-12-11 09:06:21 -0600

    Making section management work like the other management pages. Edit is the only one currently working

--- SchedulerWebsite/urls.py
+++ SchedulerWebsite/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 
 from TAScheduler.views import Home, CourseManagement, CreateCourse, EditCourse, UserAssignments, \
     Login, AccountManagement, CreateAccount, EditAccount, SectionManagement, CreateSection, EditSection, \
-    AddUserToSection, DeleteSection, Error, Success, Forgot_Password, ChooseSectionForUser
+    AddUserToSection, DeleteSection, Forgot_Password, ChooseSectionForUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,8 +39,6 @@ urlpatterns = [
     path('home/managesection/edit/', EditSection.as_view()),
     path('home/managesection/adduser/', AddUserToSection.as_view()),
     path('home/managesection/adduser/choosesection/', ChooseSectionForUser.as_view()),
-    path('home/error/', Error.as_view()),
-    path('home/success/', Success.as_view()),
     path('forgot_password/', Forgot_Password.as_view())
 ]
 
