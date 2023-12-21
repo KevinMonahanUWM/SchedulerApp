f5153bc2a3d92b6f75abb7f1516804ed57a39d9f
kevin <monahank@uwm.edu>
2023-12-20 21:43:17 -0600

    Fixed acceptance test

--- TAScheduler/AcceptanceTests/test_view_course_assignments.py
+++ TAScheduler/AcceptanceTests/test_view_course_assignments.py
@@ -6,7 +6,7 @@ class TestViewCourseAssignments(TestCase):
         self.client = Client()
 
         # Create a user using the custom User model fields.
-        self.user = User(
+        self.user = User.objects.create(
             email_address='instructor@example.com',
             password='password',  # Normally, you'd hash this password
             first_name='Instructor',
@@ -14,11 +14,12 @@ class TestViewCourseAssignments(TestCase):
             home_address='123 Test St',
             phone_number=1234567890
         )
-        self.user.save()
 
         # Create an Instructor instance associated with the user.
         self.instructor = Instructor.objects.create(user=self.user)
-
+        ses = self.client.session
+        ses["user"] = str(self.instructor)
+        ses.save()
         # Create a course.
         self.course = Course.objects.create(
             course_id=101,
@@ -35,10 +36,9 @@ class TestViewCourseAssignments(TestCase):
 
 
     def test_instructor_can_view_assignments(self):
-        url = reverse('home:managecourse:assignuser')
 
-        response = self.client.get(url)
+        response = self.client.post("/home/managecourse/", {'course': self.course, 'details': "Detail"})
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.course.name)
-        self.assertContains(response, self.course.code)
+        self.assertContains(response, self.course.course_id)
